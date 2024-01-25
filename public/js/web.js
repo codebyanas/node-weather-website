e61a2b80bf9d21c6883dcab69c6ee111eff35de2
unknown <m.anaskhalid5@gmail.com>
2024-01-25 12:57:57 +0500

    Remove  unnecessary console.log

--- public/js/web.js
+++ public/js/web.js
@@ -1,5 +1,3 @@
-console.log('Client side JavaScript only!')
-
 const weatherForm = document.querySelector('form')
 const searchInput = document.querySelector('input')
 const messageOne = document.querySelector('#message-1')
